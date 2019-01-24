df07eb699ea3a7bfbdd1a59979f54f39fd43fd2c
sakomws <43357548+sakomws@users.noreply.github.com>
2019-01-23 22:29:30 -0800

    Update install.sh

--- JH_RestAPI/install.sh
+++ JH_RestAPI/install.sh
@@ -2,8 +2,8 @@
 initdb /usr/local/var/postgres
 
 # Create db user and database with owner privileges
-psql postgres -c "CREATE USER jobhaxuser WITH PASSWORD '123456';"
-psql postgres -c "CREATE DATABASE jobhaxdb WITH OWNER 'jobhaxuser';"
+psql postgres -c "CREATE USER jobhaxdbuser WITH PASSWORD '123456';"
+psql postgres -c "CREATE DATABASE jobhaxdb WITH OWNER 'jobhaxdbuser';"
 psql postgres -c "ALTER USER jobhaxuser CREATEDB;"
 
 # Install python dependencies for application:
