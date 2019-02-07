ddeb1dfac8e98b8c429359ac094dcbcbb6f793c3
zehrasubas <zehrasubas@gmail.com>
2019-02-06 23:22:50 -0800

    job apps list

--- JH_RestAPI/jobapps/views.py
+++ JH_RestAPI/jobapps/views.py
@@ -7,9 +7,11 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view, permission_classes
 from django.db import models
 from .models import JobApplication
+from .models import JobPostDetail
 from .models import ApplicationStatus
 from .serializers import JobApplicationSerializer
 from .serializers import ApplicationStatusSerializer
+from .serializers import JobAppllicationDetailSerializer
 
 # Create your views here.
 @csrf_exempt
@@ -19,7 +21,7 @@ def get_jobapps(request):
     if status_id is not None:
         user_job_apps = JobApplication.objects.filter(applicationStatus_id=status_id,user_id=request.user.id, isDeleted=False).order_by('-applyDate')
     else:
-        user_job_apps = JobApplication.objects.filter(user_id=request.user.id, isDeleted=False).order_by('-applyDate')    
+        user_job_apps = JobApplication.objects.filter(user_id=request.user.id, isDeleted=False).order_by('-applyDate')
     joblist = JobApplicationSerializer(instance=user_job_apps, many=True).data
     return JsonResponse(create_response(joblist), safe=False)
 
@@ -28,7 +30,7 @@ def get_jobapps(request):
 def get_statuses(request):
     statuses = ApplicationStatus.objects.all()
     slist = ApplicationStatusSerializer(instance=statuses, many=True).data
-    return JsonResponse(create_response(slist), safe=False)    
+    return JsonResponse(create_response(slist), safe=False)
 
 @csrf_exempt
 @api_view(["POST"])
@@ -64,13 +66,13 @@ def update_jobapp(request):
                     else:
                         user_job_app.applicationStatus = new_status[0]
                         user_job_app.isRejected = rejected
-            user_job_app.save()        
+            user_job_app.save()
     return JsonResponse(create_response(None, success, code), safe=False)
 
 @csrf_exempt
 @api_view(["POST"])
 def delete_jobapp(request):
-    jobapp_id = request.POST.get('jobapp_id')    
+    jobapp_id = request.POST.get('jobapp_id')
     success = True
     code = 0
     if jobapp_id is None:
@@ -83,9 +85,9 @@ def delete_jobapp(request):
             code = 11
         else:
             user_job_app = user_job_app[0]
-            user_job_app.isDeleted = True    
+            user_job_app.isDeleted = True
             user_job_app.save()
-    return JsonResponse(create_response(None, success, code), safe=False)        
+    return JsonResponse(create_response(None, success, code), safe=False)
 
 @csrf_exempt
 @api_view(["POST"])
@@ -99,4 +101,22 @@ def add_jobapp(request):
     japp = JobApplication(jobTitle=job_title, company=company, applyDate=applicationdate, msgId='', source =source, user = request.user, companyLogo = None)
     japp.applicationStatus = ApplicationStatus.objects.get(pk=status)
     japp.save()
-    return JsonResponse(create_response(None), safe=False) 
+    return JsonResponse(create_response(None), safe=False)
+
+
+@csrf_exempt
+@api_view(["GET"])
+def get_jobapp_detail(request):
+  id = request.GET.get('jopapp_id')
+  try:
+    app = JobApplication.objects.filter(pk = id)
+    details = JobPostDetail.objects.filter(job_post = app)
+    print(details)
+    jobapp = JobAppllicationDetailSerializer(instance=details[0], many=True).data
+    success=True
+    code=0
+  except:
+    success=False
+    code=11
+    jobapp=None
+  return JsonResponse(create_response(jobapp,success,code), safe=False)
