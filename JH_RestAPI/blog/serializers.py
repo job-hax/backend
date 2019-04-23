7b8126e013ef4b847f30fb011a30f8b5ad060bc6
Seyfullah Demirci <sdemirci55@gmail.com>
2019-04-23 01:20:17 -0700

    timezone fix

--- JH_RestAPI/blog/serializers.py
+++ JH_RestAPI/blog/serializers.py
@@ -1,14 +1,25 @@
 from rest_framework import serializers
 from . import models
+import pytz
 
 
 class BlogSerializer(serializers.ModelSerializer):
+
+    def get_created_at(self, obj):
+        if obj.date is None:
+            return None
+        return obj.date.astimezone(pytz.timezone('US/Pacific')) 
 
     class Meta:
         fields = ('id', 'title', 'content', 'image', 'is_html', 'created_at',)
         model = models.Blog
 
 class BlogSnippetSerializer(serializers.ModelSerializer):
+
+    def get_created_at(self, obj):
+        if obj.date is None:
+            return None
+        return obj.date.astimezone(pytz.timezone('US/Pacific')) 
 
     class Meta:
         fields = ('id', 'title', 'snippet', 'image', 'created_at',)
--- JH_RestAPI/poll/serializers.py
+++ JH_RestAPI/poll/serializers.py
@@ -2,6 +2,7 @@ from .models import Poll
 from .models import Item
 from .models import Vote
 from rest_framework import serializers
+import pytz
 
 class ItemSerializer(serializers.ModelSerializer):
   def create(self, validated_data):
@@ -14,6 +15,12 @@ class PollSerializer(serializers.ModelSerializer):
   items = ItemSerializer(instance=serializers.PrimaryKeyRelatedField(many=True, read_only=True), many=True)
   def create(self, validated_data):
         return Poll.objects.create(**validated_data)
+
+  def get_date(self, obj):
+    if obj.date is None:
+      return None
+    return obj.date.astimezone(pytz.timezone('US/Pacific')) 
+
   class Meta:
     model = Poll
     fields = ('id', 'title', 'date', 'is_published', 'items')
