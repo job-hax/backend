0f8778cb2a25ae4a00411ff9bf94f60fe6b4087a
Seyfullah Demirci <sdemirci55@gmail.com>
2019-04-27 01:12:57 -0700

    create_user bugfix

--- JH_RestAPI/JH_RestAPI/settings.py
+++ JH_RestAPI/JH_RestAPI/settings.py
@@ -104,6 +104,7 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_user',
     'social_core.pipeline.user.get_username',
     'social_core.pipeline.social_auth.associate_by_email',
+    'social_core.pipeline.user.create_user',
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
