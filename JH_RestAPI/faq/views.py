55e6772cefca1821e3be3679546d91be4db88d3b
Seyfullah Demirci <sdemirci55@gmail.com>
2019-04-29 12:57:51 -0700

    faq token requirement removed

--- JH_RestAPI/faq/views.py
+++ JH_RestAPI/faq/views.py
@@ -7,10 +7,9 @@ from django.http import JsonResponse
 from utils.generic_json_creator import create_response
 from .models import Faq
 from .serializers import FaqSerializer
+from django.views.decorators.http import require_GET
 
-
-@csrf_exempt
-@api_view(["GET"])
+@require_GET
 def faqs(request):
     faq = Faq.objects.filter(is_published=True)
     slist = FaqSerializer(instance=faq, many=True).data
