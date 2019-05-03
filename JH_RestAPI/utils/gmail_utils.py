65ab433e0b11812fe4a2b46a19b0c21658423923
Seyfullah Demirci <sdemirci55@gmail.com>
2019-05-03 15:29:43 -0700

    parser improvements

--- JH_RestAPI/utils/gmail_utils.py
+++ JH_RestAPI/utils/gmail_utils.py
@@ -37,3 +37,37 @@ def find_nth(string, substring, n):
        return string.find(substring)
    else:
        return string.find(substring, find_nth(string, substring, n - 1) + 1)
+
+def unicodetoascii(text):
+
+    TEXT = (text.
+    		replace('\\xe2\\x80\\x99', "'").
+            replace('\\xc3\\xa9', 'e').
+            replace('\\xe2\\x80\\x90', '-').
+            replace('\\xe2\\x80\\x91', '-').
+            replace('\\xe2\\x80\\x92', '-').
+            replace('\\xe2\\x80\\x93', '-').
+            replace('\\xe2\\x80\\x94', '-').
+            replace('\\xe2\\x80\\x94', '-').
+            replace('\\xe2\\x80\\x98', "'").
+            replace('\\xe2\\x80\\x9b', "'").
+            replace('\\xe2\\x80\\x9c', '"').
+            replace('\\xe2\\x80\\x9c', '"').
+            replace('\\xe2\\x80\\x9d', '"').
+            replace('\\xe2\\x80\\x9e', '"').
+            replace('\\xe2\\x80\\x9f', '"').
+            replace('\\xe2\\x80\\xa6', '...').#
+            replace('\\xe2\\x80\\xb2', "'").
+            replace('\\xe2\\x80\\xb3', "'").
+            replace('\\xe2\\x80\\xb4', "'").
+            replace('\\xe2\\x80\\xb5', "'").
+            replace('\\xe2\\x80\\xb6', "'").
+            replace('\\xe2\\x80\\xb7', "'").
+            replace('\\xe2\\x81\\xba', "+").
+            replace('\\xe2\\x81\\xbb', "-").
+            replace('\\xe2\\x81\\xbc', "=").
+            replace('\\xe2\\x81\\xbd', "(").
+            replace('\\xe2\\x81\\xbe', ")")
+
+                 )
+    return TEXT       
