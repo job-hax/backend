cd08207c26e1f759c3f1016589deb91ac145e1d9
Seyfullah Demirci <sdemirci55@gmail.com>
2019-08-07 13:40:07 -0700

    bugfix

--- JH_RestAPI/position/utils.py
+++ JH_RestAPI/position/utils.py
@@ -2,6 +2,7 @@ from position.models import JobPosition
 
 
 def get_or_insert_position(title):
-    jt = JobPosition.objects.all().get_or_create(job_title__iexact=title)
+    jt, new = JobPosition.objects.all().get_or_create(job_title__iexact=title)
+    jt.job_title = title
     jt.save()
     return jt
