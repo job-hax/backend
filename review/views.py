2832b40db201c63e5e233c5a834c44d22c0df582
Seyfullah Demirci <sdemirci55@gmail.com>
2019-08-23 19:02:01 -0700

    review endpoints simplified

--- review/views.py
+++ review/views.py
@@ -17,8 +17,8 @@ from .serializers import ReviewSerializer
 
 
 @csrf_exempt
-@api_view(["POST"])
-def add_or_update_review(request):
+@api_view(["GET", "POST", "PUT"])
+def reviews(request):
     body = request.data
     user = request.user
     if 'recaptcha_token' in body and utils.verify_recaptcha(user, body['recaptcha_token'],
@@ -26,108 +26,107 @@ def add_or_update_review(request):
         return JsonResponse(create_response(data=None, success=False, error_code=ResponseCodes.verify_recaptcha_failed),
                             safe=False)
 
-    if 'company_id' not in body or 'position_id' not in body:
-        return JsonResponse(create_response(data=None, success=False, error_code=ResponseCodes.invalid_parameters),
-                            safe=False)
-    company = Company.objects.get(pk=body['company_id'])
-    position = JobPosition.objects.get(pk=body['position_id'])
-    if 'review_id' in body:
-        review = Review.objects.get(pk=body['review_id'])
-        if review.user.pk != user.pk:
-            return JsonResponse(create_response(data=None, success=False, error_code=ResponseCodes.record_not_found),
+    if request.method == "GET":
+        company_id = request.GET.get('company_id')
+        position_id = request.GET.get('position_id')
+        all_reviews = request.GET.get('all_reviews')
+        review_id = request.GET.get('review_id')
+        if review_id is not None:
+            reviews_list = Review.objects.filter(pk=review_id, user=request.user)
+            if reviews_list.count() == 0:
+                return JsonResponse(
+                    create_response(data=None, success=False, error_code=ResponseCodes.record_not_found),
+                    safe=False)
+            return JsonResponse(create_response(data=ReviewSerializer(instance=reviews_list[0], many=False).data),
                                 safe=False)
-    else:
-        review = Review()
-    review.company = company
-    review.position = position
-    review.user = request.user
-    if 'pros' in body:
-        review.pros = body['pros']
-    if 'cons' in body:
-        review.cons = body['cons']
-    if 'interview_notes' in body:
-        review.interview_notes = body['interview_notes']
-    if 'overall_company_experience' in body:
-        review.overall_company_experience = body['overall_company_experience']
-    if 'interview_difficulty' in body:
-        review.interview_difficulty = body['interview_difficulty']
-    if 'overall_interview_experience' in body:
-        review.overall_interview_experience = body['overall_interview_experience']
-    if 'anonymous' in body:
-        review.anonymous = body['anonymous']
-    if 'emp_auths' in body:
-        for a in body['emp_auths']:
-            if 'value' in a:
-                auth = EmploymentAuth.objects.get(pk=a['id'])
-                review.save()
-                if CompanyEmploymentAuth.objects.filter(review=review, employment_auth=auth).count() == 0:
-                    c_auth = CompanyEmploymentAuth(
-                        review=review, employment_auth=auth, value=a['value'])
-                    c_auth.save()
-                else:
-                    c_auth = CompanyEmploymentAuth.objects.get(
-                        review=review, employment_auth=auth)
-                    c_auth.value = a['value']
-                    c_auth.save()
-    if 'emp_status_id' in body:
-        review.emp_status = EmploymentStatus.objects.get(
-            pk=body['emp_status_id'])
-    if 'source_type_id' in body:
-        review.source_type = SourceType.objects.get(pk=body['source_type_id'])
-
-    # publish review if there is no content to approve
-    if 'pros' not in body and 'cons' not in body and 'interview_notes' not in body:
-        review.is_published = True
-    else:
-        review.is_published = False
-
-    review.save()
-    response = {}
-    response['review'] = ReviewSerializer(instance=review, many=False).data
-    response['company'] = CompanySerializer(instance=company, many=False, context={
-        'user': request.user, 'position': position}).data
-    return JsonResponse(create_response(data=response), safe=False)
-
-
-@csrf_exempt
-@api_view(["GET"])
-def get_reviews(request):
-    company_id = request.GET.get('company_id')
-    position_id = request.GET.get('position_id')
-    all_reviews = request.GET.get('all_reviews')
-    review_id = request.GET.get('review_id')
-    if review_id is not None:
-        reviews = Review.objects.filter(pk=review_id, user=request.user)
-        if reviews.count() == 0:
-            return JsonResponse(create_response(data=None, success=False, error_code=ResponseCodes.record_not_found),
+        elif company_id is None and position_id is None:
+            return JsonResponse(create_response(data=None, success=False, error_code=ResponseCodes.invalid_parameters),
                                 safe=False)
-        return JsonResponse(create_response(data=ReviewSerializer(instance=reviews[0], many=False).data), safe=False)
-    elif company_id is None and position_id is None:
-        return JsonResponse(create_response(data=None, success=False, error_code=ResponseCodes.invalid_parameters),
-                            safe=False)
-    if company_id is None:
-        reviews = Review.objects.filter(Q(is_published=True) | Q(
-            user=request.user), position__pk=position_id)
-    elif position_id is None:
-        reviews = Review.objects.filter(Q(is_published=True) | Q(
-            user=request.user), company__pk=company_id)
-    else:
-        if all_reviews == 'true':
-            reviews = Review.objects.filter(
-                is_published=True, position__pk=position_id, company__pk=company_id)
+        if company_id is None:
+            reviews_list = Review.objects.filter(Q(is_published=True) | Q(
+                user=request.user), position__pk=position_id)
+        elif position_id is None:
+            reviews_list = Review.objects.filter(Q(is_published=True) | Q(
+                user=request.user), company__pk=company_id)
         else:
-            reviews = Review.objects.filter(
-                user=request.user, position__pk=position_id, company__pk=company_id)
-            if reviews.count() > 0:
-                return JsonResponse(create_response(data=ReviewSerializer(instance=reviews[0], many=False).data),
-                                    safe=False)
+            if all_reviews == 'true':
+                reviews_list = Review.objects.filter(
+                    is_published=True, position__pk=position_id, company__pk=company_id)
             else:
-                return JsonResponse(create_response(data=None), safe=False)
-    return JsonResponse(create_response(data=ReviewSerializer(instance=reviews, many=True).data), safe=False)
+                reviews_list = Review.objects.filter(
+                    user=request.user, position__pk=position_id, company__pk=company_id)
+                if reviews_list.count() > 0:
+                    return JsonResponse(create_response(data=ReviewSerializer(instance=reviews_list[0], many=False).data),
+                                        safe=False)
+                else:
+                    return JsonResponse(create_response(data=None), safe=False)
+        return JsonResponse(create_response(data=ReviewSerializer(instance=reviews_list, many=True).data), safe=False)
+    else:
+        if 'company_id' not in body or 'position_id' not in body:
+            return JsonResponse(create_response(data=None, success=False, error_code=ResponseCodes.invalid_parameters),
+                                safe=False)
+
+        company = Company.objects.get(pk=body['company_id'])
+        position = JobPosition.objects.get(pk=body['position_id'])
+        if request.method == "PUT":
+            review = Review.objects.get(pk=body['review_id'])
+            if review.user.pk != user.pk:
+                return JsonResponse(create_response(data=None, success=False, error_code=ResponseCodes.record_not_found),
+                                    safe=False)
+        elif request.method == "POST":
+            review = Review()
+        review.company = company
+        review.position = position
+        review.user = request.user
+        if 'pros' in body:
+            review.pros = body['pros']
+        if 'cons' in body:
+            review.cons = body['cons']
+        if 'interview_notes' in body:
+            review.interview_notes = body['interview_notes']
+        if 'overall_company_experience' in body:
+            review.overall_company_experience = body['overall_company_experience']
+        if 'interview_difficulty' in body:
+            review.interview_difficulty = body['interview_difficulty']
+        if 'overall_interview_experience' in body:
+            review.overall_interview_experience = body['overall_interview_experience']
+        if 'anonymous' in body:
+            review.anonymous = body['anonymous']
+        if 'emp_auths' in body:
+            for a in body['emp_auths']:
+                if 'value' in a:
+                    auth = EmploymentAuth.objects.get(pk=a['id'])
+                    review.save()
+                    if CompanyEmploymentAuth.objects.filter(review=review, employment_auth=auth).count() == 0:
+                        c_auth = CompanyEmploymentAuth(
+                            review=review, employment_auth=auth, value=a['value'])
+                        c_auth.save()
+                    else:
+                        c_auth = CompanyEmploymentAuth.objects.get(
+                            review=review, employment_auth=auth)
+                        c_auth.value = a['value']
+                        c_auth.save()
+        if 'emp_status_id' in body:
+            review.emp_status = EmploymentStatus.objects.get(
+                pk=body['emp_status_id'])
+        if 'source_type_id' in body:
+            review.source_type = SourceType.objects.get(pk=body['source_type_id'])
+
+        # publish review if there is no content to approve
+        if 'pros' not in body and 'cons' not in body and 'interview_notes' not in body:
+            review.is_published = True
+        else:
+            review.is_published = False
+
+        review.save()
+        response = {'review': ReviewSerializer(instance=review, many=False).data,
+                    'company': CompanySerializer(instance=company, many=False, context={
+                        'user': request.user, 'position': position}).data}
+        return JsonResponse(create_response(data=response), safe=False)
 
 
 @csrf_exempt
 @api_view(["GET"])
-def get_source_types(request):
+def source_types(request):
     sources = SourceType.objects.all()
     return JsonResponse(create_response(data=SourceSerializer(instance=sources, many=True).data), safe=False)
