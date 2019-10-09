dc486e858c430b3e1abf4be2b5bc0ca387335bc4
Seyfullah Demirci <sdemirci55@gmail.com>
2019-10-09 15:21:43 -0700

    alumni homepage endpoint

--- alumni/admin.py
+++ alumni/admin.py
@@ -1,1 +1,9 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import AlumniHomePage
+
+
+@admin.register(AlumniHomePage)
+class CollegeCoachAdmin(admin.ModelAdmin):
+    list_display = ("college", "id")
+    list_filter = ("college", "id")
--- alumni/models.py
+++ alumni/models.py
@@ -1,1 +1,13 @@
-# Create your models here.
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from django.contrib.postgres.fields import JSONField
+
+from college.models import College
+
+
+class AlumniHomePage(models.Model):
+    college = models.ForeignKey(College, on_delete=models.CASCADE, null=False, blank=False)
+    banners = ArrayField(JSONField(null=True, blank=True), blank=True, null=True)
+    secondary_banners = ArrayField(JSONField(null=True, blank=True), blank=True, null=True)
+    social_accounts = ArrayField(JSONField(null=True, blank=True), blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
