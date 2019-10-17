3cbd54e16ae2e05bd8dbd33bf2547d26711ec02d
Yonglin Zhong <zhongyonglin@hotmail.com>
2019-10-17 13:12:08 -0700

    add back the jobapps module

---
+++ jobapps/models.py
@@ -0,0 +1,120 @@
+from django.utils import timezone
+
+from django.contrib.auth import get_user_model
+from django.core.validators import RegexValidator
+from django.db import models
+from django.utils.translation import gettext as _
+
+from company.models import Company
+from position.models import JobPosition
+
+User = get_user_model()
+
+
+class ApplicationStatus(models.Model):
+    value = models.CharField(max_length=20)
+    icon = models.FileField(blank=True, null=True)
+    pos = models.SmallIntegerField(default='0', verbose_name=_('position'))
+    rejectable = models.BooleanField(default=True)
+    default = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['value']
+        verbose_name = _('status')
+        verbose_name_plural = _('statuses')
+        ordering = ['pos']
+
+    def __str__(self):
+        return self.value if self.value is not None else ''
+
+
+class Source(models.Model):
+    value = models.CharField(max_length=20)
+    gmail_key = models.CharField(max_length=100, blank=True)
+    system = models.BooleanField(default=False)
+    image = models.CharField(null=True, max_length=200, blank=True)
+
+    class Meta:
+        ordering = ['value']
+
+    def __str__(self):
+        return self.value if self.value is not None else ''
+
+
+class JobApplication(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True)
+    application_status = models.ForeignKey(
+        ApplicationStatus, on_delete=models.SET_NULL, null=True, blank=True, related_name='%(class)s_application_status')
+    position = models.ForeignKey(
+        JobPosition, on_delete=models.SET_NULL, null=True, related_name='%(class)s_position')
+    company_object = models.ForeignKey(
+        Company, on_delete=models.SET_NULL, null=True, related_name='%(class)s_company')
+    apply_date = models.DateTimeField(blank=True)
+    msg_id = models.CharField(max_length=200)
+    app_source = models.ForeignKey(
+        Source, on_delete=models.SET_NULL, null=True, related_name='%(class)s_source')
+    rejected_date = models.DateTimeField(null=True, blank=True)
+    deleted_date = models.DateTimeField(null=True, blank=True)
+    created = models.DateTimeField(default=timezone.now, null=True, blank=True)
+    updated_date = models.DateTimeField(null=True, blank=True)
+    is_rejected = models.BooleanField(default=False)
+    is_deleted = models.BooleanField(default=False)
+
+
+class StatusHistory(models.Model):
+    job_post = models.ForeignKey(
+        JobApplication, on_delete=models.CASCADE, null=True, blank=True)
+    application_status = models.ForeignKey(
+        ApplicationStatus, on_delete=models.SET_NULL, null=True, blank=True, related_name='%(class)s_application_status')
+    update_date = models.DateTimeField(default=timezone.now, blank=True)
+
+
+class GoogleMail(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=True)
+    job_post = models.ForeignKey(
+        JobApplication, on_delete=models.CASCADE, null=True, blank=True)
+    app_source = models.ForeignKey(
+        Source, on_delete=models.SET_NULL, null=True, related_name='%(class)s_source')
+    subject = models.CharField(max_length=200)
+    body = models.TextField(null=True, blank=True)
+    date = models.CharField(max_length=50)
+    msg_id = models.CharField(max_length=200, null=True)
+
+
+class JobApplicationNote(models.Model):
+    job_post = models.ForeignKey(
+        JobApplication, on_delete=models.CASCADE, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    created_date = models.DateTimeField(default=timezone.now, blank=True)
+    update_date = models.DateTimeField(
+        default=timezone.now, null=True, blank=True)
+
+
+class SourceType(models.Model):
+    value = models.CharField(max_length=20, null=False)
+
+    def __str__(self):
+        return self.value if self.value is not None else ''
+
+
+class Contact(models.Model):
+    job_post = models.ForeignKey(
+        JobApplication, on_delete=models.CASCADE, null=True, blank=True)
+    first_name = models.CharField(max_length=50, null=True, blank=True)
+    last_name = models.CharField(max_length=50, null=True, blank=True)
+    email = models.CharField(max_length=150, null=True, blank=True)
+    phone_regex = RegexValidator(
+        regex=r'^\+?1?\d{9,15}$',
+        message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(
+        validators=[phone_regex], max_length=17, blank=True, null=True)  # validators should be a list
+    linkedin_url = models.CharField(max_length=100, blank=True, null=True)
+    position = models.ForeignKey(
+        JobPosition, on_delete=models.SET_NULL, null=True, related_name='%(class)s_position')
+    company = models.ForeignKey(
+        Company, on_delete=models.SET_NULL, null=True, related_name='%(class)s_company')
+    description = models.TextField(null=True, blank=True)
+    created_date = models.DateTimeField(default=timezone.now, blank=True)
+    update_date = models.DateTimeField(
+        default=timezone.now, null=True, blank=True)
---
+++ jobapps/serializers.py
@@ -0,0 +1,130 @@
+import pytz
+from rest_framework import serializers
+
+from company.serializers import CompanySerializer
+from position.serializers import JobPositionSerializer
+from .models import ApplicationStatus
+from .models import GoogleMail
+from .models import JobApplication
+from .models import JobApplicationNote, SourceType
+from .models import Source, Contact
+from .models import StatusHistory
+
+
+class ApplicationStatusSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        return ApplicationStatus.objects.create(**validated_data)
+
+    class Meta:
+        model = ApplicationStatus
+        fields = ('__all__')
+
+
+class SourceTypeSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        return SourceType.objects.create(**validated_data)
+
+    class Meta:
+        model = SourceType
+        fields = ('__all__')
+
+
+class GoogleMailSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        return GoogleMail.objects.create(**validated_data)
+
+    class Meta:
+        model = GoogleMail
+        fields = ('subject', 'body', 'date')
+
+
+class SourceSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        return Source.objects.create(**validated_data)
+
+    class Meta:
+        model = Source
+        fields = ('id', 'value', 'image')
+
+
+class JobApplicationSerializer(serializers.ModelSerializer):
+    application_status = ApplicationStatusSerializer(read_only=True)
+    position = JobPositionSerializer(read_only=True)
+    company_object = serializers.SerializerMethodField()
+    app_source = SourceSerializer(read_only=True)
+    editable = serializers.SerializerMethodField()
+
+    def get_editable(self, obj):
+        if obj.msg_id is not None and obj.msg_id != '':
+            return False
+        return True
+
+    def get_company_object(self, obj):
+        context = self.context
+        context['position'] = obj.position
+        return CompanySerializer(instance=obj.company_object, many=False, context=context).data
+
+    def create(self, validated_data):
+        return JobApplication.objects.create(**validated_data)
+
+    class Meta:
+        model = JobApplication
+        fields = (
+            'id', 'editable', 'application_status', 'position', 'company_object', 'apply_date', 'app_source', 'is_rejected')
+
+
+class JobApplicationNoteSerializer(serializers.ModelSerializer):
+    created_date = serializers.SerializerMethodField()
+    update_date = serializers.SerializerMethodField()
+
+    def get_created_date(self, obj):
+        if obj.created_date is None:
+            return None
+        return obj.created_date.astimezone(pytz.timezone('US/Pacific'))
+
+    def get_update_date(self, obj):
+        if obj.update_date is None:
+            return None
+        return obj.update_date.astimezone(pytz.timezone('US/Pacific'))
+
+    def create(self, validated_data):
+        return JobApplicationNote.objects.create(**validated_data)
+
+    class Meta:
+        model = JobApplicationNote
+        fields = ('id', 'description', 'created_date', 'update_date')
+
+
+class StatusHistorySerializer(serializers.ModelSerializer):
+    application_status = ApplicationStatusSerializer(read_only=True)
+
+    def create(self, validated_data):
+        return StatusHistory.objects.create(**validated_data)
+
+    class Meta:
+        model = StatusHistory
+        fields = ('application_status', 'update_date')
+
+
+class ContactSerializer(serializers.ModelSerializer):
+    position = serializers.SerializerMethodField()
+    company = serializers.SerializerMethodField()
+
+    def get_position(self, obj):
+        if obj.position is not None:
+            return obj.position.job_title
+        return None
+
+    def get_company(self, obj):
+        if obj.company is not None:
+            return obj.company.company
+        return None
+
+    def create(self, validated_data):
+        return Contact.objects.create(**validated_data)
+
+    class Meta:
+        model = Contact
+        fields = (
+            'id', 'first_name', 'last_name', 'phone_number', 'linkedin_url', 'description', 'created_date', 'update_date', 'position',
+            'company', 'email')
